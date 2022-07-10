c024e46e6abb90d17a43dbd033c7941c5fabfc27
Joshua Robison <joshua-robison@outlook.com>
2022-07-09 22:52:29 -0400

    Update mnist.py

--- notebooks/mnist.py
+++ notebooks/mnist.py
@@ -10,39 +10,49 @@ import numpy as np
 from urllib.request import urlretrieve
 
 
-#from: https://github.com/datapythonista/mnist
-DATASET_DIRECTORY = '../data/'
-URL = 'http://yann.lecun.com/exdb/mnist/'
+# from: https://github.com/datapythonista/mnist
+DATASET_DIRECTORY = "../data/"
+URL = "http://yann.lecun.com/exdb/mnist/"
 
 
 def parse_idx(fd):
-    DATA_TYPES = {0x08: 'B',  # unsigned byte
-                  0x09: 'b',  # signed byte
-                  0x0b: 'h',  # short (2 bytes)
-                  0x0c: 'i',  # int (4 bytes)
-                  0x0d: 'f',  # float (4 bytes)
-                  0x0e: 'd'}  # double (8 bytes)
+    DATA_TYPES = {
+        0x08: "B",  # unsigned byte
+        0x09: "b",  # signed byte
+        0x0B: "h",  # short (2 bytes)
+        0x0C: "i",  # int (4 bytes)
+        0x0D: "f",  # float (4 bytes)
+        0x0E: "d",
+    }  # double (8 bytes)
     header = fd.read(4)
     if len(header) != 4:
-        raise IdxDecodeError('Invalid IDX file, file empty or does not contain a full header.')
+        raise IdxDecodeError(
+            "Invalid IDX file, file empty or does not contain a full header."
+        )
 
-    zeros, data_type, num_dimensions = struct.unpack('>HBB', header)
+    zeros, data_type, num_dimensions = struct.unpack(">HBB", header)
     if zeros != 0:
-        raise IdxDecodeError('Invalid IDX file, file must start with two zero bytes. '
-                             'Found 0x%02x' % zeros)
-    
+        raise IdxDecodeError(
+            "Invalid IDX file, file must start with two zero bytes. "
+            "Found 0x%02x" % zeros
+        )
+
     try:
         data_type = DATA_TYPES[data_type]
     except KeyError:
-        raise IdxDecodeError('Unknown data type 0x%02x in IDX file' % data_type)
-        
-    dimension_sizes = struct.unpack('>' + 'I' * num_dimensions, fd.read(4 * num_dimensions))
+        raise IdxDecodeError("Unknown data type 0x%02x in IDX file" % data_type)
+
+    dimension_sizes = struct.unpack(
+        ">" + "I" * num_dimensions, fd.read(4 * num_dimensions)
+    )
     data = array.array(data_type, fd.read())
     data.byteswap()
     expected_items = functools.reduce(operator.mul, dimension_sizes)
     if len(data) != expected_items:
-        raise IdxDecodeError('IDX file has wrong number of items. '
-                             'Expected: %d. Found: %d' % (expected_items, len(data)))
+        raise IdxDecodeError(
+            "IDX file has wrong number of items. "
+            "Expected: %d. Found: %d" % (expected_items, len(data))
+        )
 
     return np.array(data).reshape(dimension_sizes)
 
@@ -50,7 +60,7 @@ def parse_idx(fd):
 def print_download_progress(count, block_size, total_size):
     pct_complete = int(count * block_size * 100 / total_size)
     pct_complete = min(pct_complete, 100)
-    msg = '\r- Download progress: %d' % (pct_complete) + '%'
+    msg = "\r- Download progress: %d" % (pct_complete) + "%"
     sys.stdout.write(msg)
     sys.stdout.flush()
 
@@ -58,29 +68,31 @@ def print_download_progress(count, block_size, total_size):
 def download_and_parse_mnist_file(fname, target_dir=None, force=False):
     if not os.path.exists(DATASET_DIRECTORY):
         os.makedirs(DATASET_DIRECTORY)
-    if not os.path.exists(DATASET_DIRECTORY+fname):
-        print('Downloading '+fname)
+    if not os.path.exists(DATASET_DIRECTORY + fname):
+        print("Downloading " + fname)
         file_path = os.path.join(DATASET_DIRECTORY, fname)
         url = URL + fname
-        file_path, _ = urlretrieve(url=url, filename=file_path, reporthook=print_download_progress)
-        print('\nDownload finished.')
-        
-    fopen = gzip.open if os.path.splitext(fname)[1] == '.gz' else open
-    with fopen(fname, 'rb') as fd:
+        file_path, _ = urlretrieve(
+            url=url, filename=file_path, reporthook=print_download_progress
+        )
+        print("\nDownload finished.")
+
+    fopen = gzip.open if os.path.splitext(fname)[1] == ".gz" else open
+    with fopen(fname, "rb") as fd:
         return parse_idx(fd)
 
 
 def train_images():
-    return download_and_parse_mnist_file('../data/train-images-idx3-ubyte.gz')
+    return download_and_parse_mnist_file("../data/train-images-idx3-ubyte.gz")
 
 
 def test_images():
-    return download_and_parse_mnist_file('../data/t10k-images-idx3-ubyte.gz')
+    return download_and_parse_mnist_file("../data/t10k-images-idx3-ubyte.gz")
 
 
 def train_labels():
-    return download_and_parse_mnist_file('../data/train-labels-idx1-ubyte.gz')
+    return download_and_parse_mnist_file("../data/train-labels-idx1-ubyte.gz")
 
 
 def test_labels():
-    return download_and_parse_mnist_file('../data/t10k-labels-idx1-ubyte.gz')
+    return download_and_parse_mnist_file("../data/t10k-labels-idx1-ubyte.gz")
